fd2ed8fe3feab4bbef16794b60c9d194e8409b62
buianhminh <bui.anh.minh99@gmail.com>
2025-02-21 22:02:33 +1100

    feat: create aws s3 bucket and iam user. add route into server.js

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ app.use(express.json());
 // Import routes
 const settingsRoutes = require("./routes/settings");
 const menuRoutes = require("./routes/enu");
+const uploadRoutes = require("./routes/upload");
 
 // Use routes
 app.use("/api/settings", settingsRoutes);
 app.use("/api/menu", menuRoutes);
+app.use("/api/upload", uploadRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
